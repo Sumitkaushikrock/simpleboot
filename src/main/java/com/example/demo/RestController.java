dcb987500ec176e3c69eb3b526f6001bbfd2d4a9
sumitkaushikrock <sumitkaushik445@gmail.com>
2023-11-06 23:55:06 +0530

    hello

--- src/main/java/com/example/demo/RestController.java
+++ src/main/java/com/example/demo/RestController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
     @GetMapping("rest/{fileName}")
-    public String getter(String fileName){
+    public String getter(@PathVariable  String fileName){
         return "String"+fileName;
     }
 }
